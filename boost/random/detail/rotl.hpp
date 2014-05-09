571fa47cb631f0b4e084589e2cdc6dbfe4b55d17
johnsalmon <john@thesalmons.org>
2014-05-09 15:00:18 -0400

    protect rotl with some static assertions

--- boost/random/detail/rotl.hpp
+++ boost/random/detail/rotl.hpp
@@ -32,11 +32,22 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #ifndef BOOST_RANDOM_DETAIL_ROTL_HPP
 #define BOOST_RANDOM_DETAIL_ROTL_HPP
 
+#include <boost/static_assert.hpp>
+#include <limits>
+
 namespace boost{
 namespace random{
 namespace detail{
 template <typename Uint>
 static Uint rotl(Uint x, unsigned s){
+    // Non-binary types would be a huge headache.  Sign-extension of
+    // right-shift would not give the intended result.  These should
+    // never come up, so it's not worth the trouble to try to handle
+    // them "correctly".  But just in case they do, let's blow an
+    // assertion rather than generating incorrect code.
+    BOOST_STATIC_ASSERT(std::numeric_limits<Uint>::is_specialized);
+    BOOST_STATIC_ASSERT(std::numeric_limits<Uint>::radix == 2);
+    BOOST_STATIC_ASSERT(!std::numeric_limits<Uint>::is_signed);
     return (x<<s) | (x>>(std::numeric_limits<Uint>::digits-s));
 }
 }
